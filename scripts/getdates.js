4d324b2af5ea4ea174ffc815ac912cf981ecb8b6
wdd13o <konuoha1@byupathway.edu>
2025-07-06 14:53:46 +0200

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically display current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display document last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
